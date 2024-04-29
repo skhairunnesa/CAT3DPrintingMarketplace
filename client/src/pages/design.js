a83000ac8caf37426bf16c9bad2f403d4e2b5dbc
cloncka <114435069+cloncka@users.noreply.github.com>
2024-04-29 17:29:08 -0500

    Add files via upload

--- client/src/pages/design.js
+++ client/src/pages/design.js
@@ -1,13 +1,33 @@
 import React from "react";
- 
+import DesignUpload from "../components/DesignUpload";
+
 const Design = () => {
-    return (
+// Parse the JSON string stored in localStorage
+const userData = JSON.parse(localStorage.user);
+
+// Access the seller subType
+const sellerSubType = userData.seller.subType;
+
+console.log("Seller subType:", sellerSubType);
+
+// Check if userType is 'designer'
+    if (sellerSubType === 'designer') {
+        return (
+            <div>
+                <DesignUpload>
+                    <b>Upload Designs</b>
+                </DesignUpload>
+            </div>
+        );
+    } else {
+        // If userType is not 'Designer', render nothing
         <div>
             <h1>
-                Insert Design Stuff Here
+                Only Designer Accounts can upload Designs
             </h1>
         </div>
-    );
+        return null;
+    }
 };
- 
+
 export default Design;
