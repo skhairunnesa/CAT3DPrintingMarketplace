98275e88bd9b54a732bdfd0e3b0fcf07fe10f641
cloncka <114435069+cloncka@users.noreply.github.com>
2024-04-29 17:05:07 -0500

    Add files via upload

--- client/src/components/DesignUpload.js
+++ client/src/components/DesignUpload.js
@@ -1,22 +1,24 @@
-/// <summary>
-/// Author: Isa Luluquisin
-/// Description: Script formats the design-upload card
-///</summary> 
+import React, { useState } from "react";
+import UploadForm from "./UploadForm"; 
+import "../assets/designUpload.css";
 
-import React from "react";
-import {Link} from 'react-router-dom'
-import "../assets/designUpload.css"
+const DesignUpload = ({ children }) => {
+  // State to track whether the upload button has been clicked
+  const [isUploadClicked, setIsUploadClicked] = useState(false);
 
-const DesignUpload = ({ image, photoText, designLink }) => {
   return (
     <div className="designUploadContainer">
-      <Link to= {designLink}>
-        <img className="designImage" src={image} alt="" />
-      </Link>
-      <div className="designText">
-        <p>{photoText}</p>
-      </div>
+        {/* Display the upload button */}
+        {!isUploadClicked && (
+          <button className="uploadbtn" onClick={() => setIsUploadClicked(true)}>
+            {children}
+          </button>
+        )}
+
+        {/* Display the form only when the upload button is clicked */}
+        {isUploadClicked && <UploadForm />}
     </div>
   );
-}
+};
+
 export default DesignUpload;
