092e7a233296bbc6be73cc31fccb76ac773a114f
Aidan Wright <49991562+theoneGUI@users.noreply.github.com>
2024-05-02 02:51:07 +0000

    Fix with absolute file path
    Also fixed a bug where it would fetch the product details forever on the individual product page.

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -39,7 +39,7 @@ const Product = () => {
           setStructureInfo(error.message);
           setDataReceived(false);
      });
-    });
+    }, []);
 
     function createDropDown(){
          let items = [];
