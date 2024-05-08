430fca084dea97c434c6bb0ead1cfd022eab90f1
eriksive2 <eriksive65@gmail.com>
2024-05-08 05:03:12 -0500

    updated checkout page
    it looks nicer, axios delete actually registers that some is deleting but it gives a 404 bc it can't find the id of the db document, api link leads to a dead page

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -27,22 +27,37 @@ const CheckoutPage = () => {
         axios.post('http://localhost:5000/orders/add', data)
             .then(response => {
                 console.log('Order submitted successfully:', response.data);
-                clearCart();
+                setCartData([]);
             })
             .catch(error => console.error('Error submitting order:', error));
         
     }
 
-    const clearCart = () => {
-        setCartData([]);
-        axios.delete('http://localhost:5000/addCart')
-        .then(response => {
-            console.log('Order submitted successfully:', response.data);
-            let newCart = cartData.filter((el) => el.orderID !== orderID);
-
-            setCartData(newCart);
-        })
-        .catch(error => console.error('Error submitting order:', error));
+    function clearCart(id){
+        axios.delete(`http://localhost:5000/addCart/${id}`, {data: {cartData}})
+            .then(response => {
+                console.log(`Deleted cart item`);
+            })
+            .catch(error => {
+                if (error.response) {
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  console.log(error.request);
+                } else {
+                  console.log('Error', error.message);
+                }
+                console.log(error.config);
+            });
+        
+        // fetch(`http://localhost:5000/addCart/${id}`,{
+        //     method: 'DELETE'
+        // }).then((result)=>{
+        //     result.json().then((resp)=>{
+        //         console.warn(resp)
+        //     })
+        // })
     }
 
     const generateOrderID = () => {
@@ -53,10 +68,11 @@ const CheckoutPage = () => {
 
     return (
         <div className="checkout-container">
-            <h1>Checkout</h1>
+            <h1 style={{marginLeft:"40%"}}>Checkout</h1>
             {cartData.length > 0 ? (
                 cartData.map(item => (
                     <div className="cart-item" key={item.user_id}>
+                        
                         <p><strong>User ID:</strong> {item.user_id}</p>
                         <p><strong>Designer:</strong> {item.designer}</p>
                         <p><strong>Product Name:</strong> {item.product_id}</p>
@@ -64,13 +80,14 @@ const CheckoutPage = () => {
                         <p><strong>Baths:</strong> {item.numBath}</p>
                         <p><strong>Quantity:</strong> {item.q}</p>
                         <p><strong>Total Cost:</strong> {item.total_cost}</p>
+                        <button onClick={() => clearCart(item.user_id)} style={{justifyContent: "flex-end"}}>Remove</button>
                     </div>
                 ))
             ) : (
                 <div>No items in your cart</div>
             )}
 
-            <button onClick={submitCartDataToOrders} style={{marginLeft:"450px",width:"150px", height:"60px", fontSize:"18px"}}>Submit Order</button>
+            <button onClick={submitCartDataToOrders} style={{width:"150px", height:"60px", fontSize:"18px"}}>Submit Order</button>
         </div>
     );
 }
