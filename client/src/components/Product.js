01d69269d93fb7658d07d597eb88cd6eddd6a74e
Ryan Banwart <126316017+ProbablyCanadian@users.noreply.github.com>
2024-05-07 20:25:05 -0500

    Update Product.js

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,17 +1,19 @@
 import React from "react";
-import "../assets/Product.css";
+import "../styles/Product.css";
 import {Link} from 'react-router-dom'
 
-const Product = ({name, image, link}) => {
+const Product = ({title, image, link, designer, price}) => {
 //make this whole thing a button/link?
     return(
         <div className="header">
-        <Link to= {link}>
-            <img className="productPhoto" src={image} alt="" />
-        </Link>
-        <div className="productTitle">
-                <p>{name}</p>
-            </div> 
+            <Link to= {link}>
+                <img className="productPhoto" src={image} alt="" />
+            </Link>
+            <div className="productInfo" >
+                <strong>{title}</strong>
+                <section><small>Designed by: {designer}</small></section>
+                <section><small>Price: ${price}</small></section>
+            </div>
         </div>
     );
 }
